a89a8d1327ca81c27087f022c0d192e4ae7cd31a
panicboy <asasaki@gmail.com>
2016-08-15 13:56:39 -1000

    38 tests passing

--- test/es6Practice.spec.js
+++ test/es6Practice.spec.js
@@ -298,7 +298,7 @@ describe('destructuring arrays makes shorter code', () => {
 
   it('extract from nested arrays', () => {
     const user = [['Some', 'One'], 23];
-    const [firstName, surname, age] = user;
+    const [[firstName, surname], age] = user;
 
     const expected = 'Some One = 23 years';
     assert.equal(`${firstName} ${surname} = ${age} years`, expected);
