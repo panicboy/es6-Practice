f9918ba07fd5aedeed0eb3a1f0a7ce4ca6819876
panicboy <asasaki@gmail.com>
2016-08-15 14:10:43 -1000

    39 tests passing

--- test/es6Practice.spec.js
+++ test/es6Practice.spec.js
@@ -306,7 +306,7 @@ describe('destructuring arrays makes shorter code', () => {
 
   it('chained assignments', () => {
     let c, d;
-    let a, b = [c, d] = [1, 2];
+    let [a, b] = [c, d] = [1, 2];
     assert.deepEqual([a, b, c, d], [1, 2, 1, 2]);
   });
 
