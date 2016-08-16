62c803ec0cd835d18c290c1b2637199e330b43ce
panicboy <asasaki@gmail.com>
2016-08-15 16:06:54 -1000

    64 tests passing

--- test/es6Practice.spec.js
+++ test/es6Practice.spec.js
@@ -528,7 +528,7 @@ describe('Symbol', function() {
   });
 
   it('`typeof Symbol()` returns "symbol"', function(){
-    const theType = typeof Symbol;
+    const theType = typeof Symbol();
     assert.equal(theType, 'symbol');
   });
 
